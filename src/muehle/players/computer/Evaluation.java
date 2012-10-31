a30893488fadf695fb97d07e06bc9df1ebf9b7ba
pege <patrick.gruntz@web.de>
2012-10-31 21:50:20 +0100

    evaluation error

--- src/muehle/players/computer/Evaluation.java
+++ src/muehle/players/computer/Evaluation.java
@@ -3,6 +3,7 @@ package muehle.players.computer;
 import static muehle.model.Board.eColor.BLACK;
 import static muehle.model.Board.eColor.WHITE;
 import muehle.model.Board;
+import muehle.model.Board.eColor;
 
 public class Evaluation {
 
@@ -13,31 +14,31 @@ public class Evaluation {
 	 *            the situation which is evaluated
 	 * @return the evaluation in the form of an integer
 	 */
-	public static int evaluation(Board board) {
+	public static int evaluation(Board board, eColor player, eColor opposite) {
 
 		int bewertung = (int) Math.round(Math.random() * 40);
 
-		int BlackMills = board.getNumberOfMills(BLACK);
-		int WhiteMills = board.getNumberOfMills(WHITE);
+		int playerMills = board.getNumberOfMills(player);
+		int oppositeMills = board.getNumberOfMills(opposite);
 
 //		int numberOfWhiteStones = board.getNumberOfStones(WHITE);
 //		int numberOfBlackStones = board.getNumberOfStones(BLACK);
 
-		int openMillsBlack = board.getNumberOfOpenMills(BLACK);
-		int openMillsWhite = board.getNumberOfOpenMills(WHITE);
+		int openMillsPlayer = board.getNumberOfOpenMills(player);
+		int openMillsOpposite = board.getNumberOfOpenMills(opposite);
 
-		if (board.getStuck(BLACK)) {
+		if (board.getStuck(player)) {
 			bewertung = bewertung - 1000;
-		} else if (board.getStuck(WHITE)) {
+		} else if (board.getStuck(opposite)) {
 			bewertung = bewertung + 1000;
 		}
-		bewertung = bewertung + 200 * openMillsBlack;
-		bewertung = bewertung - 200 * openMillsWhite;
+		bewertung = bewertung + 200 * openMillsPlayer;
+		bewertung = bewertung - 200 * openMillsOpposite;
 
-		if (BlackMills > WhiteMills) {
-			bewertung = bewertung + 100 * (BlackMills - WhiteMills);
-		} else if (BlackMills < WhiteMills) {
-			bewertung = bewertung + 200 * (BlackMills - WhiteMills);
+		if (playerMills > oppositeMills) {
+			bewertung = bewertung + 100 * (playerMills - oppositeMills);
+		} else if (playerMills < oppositeMills) {
+			bewertung = bewertung + 200 * (playerMills - oppositeMills);
 		}
 
 		return bewertung;
