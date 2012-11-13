9d4ebbfa69f12834e501318685b31c194bf88a96
pege <patrick.gruntz@web.de>
2012-11-13 11:55:36 +0100

    comments 2

--- src/muehle/players/computer/ComputerPlayer.java
+++ src/muehle/players/computer/ComputerPlayer.java
@@ -1,6 +1,5 @@
 package muehle.players.computer;
 
-import muehle.Linker;
 import muehle.model.Board;
 import muehle.model.Board.StoneColor;
 import muehle.players.Move;
@@ -11,9 +10,10 @@ public class ComputerPlayer implements NineMenMorrisPlayer {
 	private String name;
 	private StoneColor player;
 	private StoneColor opposite;
-	
+
 	/**
-	 * @param name is the Name of the Computer
+	 * @param name
+	 *            is the Name of the Computer
 	 */
 	public ComputerPlayer(String name) {
 		this.name = name;
@@ -23,33 +23,35 @@ public class ComputerPlayer implements NineMenMorrisPlayer {
 		return name;
 	}
 
-
 	@Override
 	public void setColor(StoneColor color) {
 		this.player = color;
-		if(color == StoneColor.BLACK) opposite = StoneColor.WHITE; else opposite = StoneColor.BLACK;
+		if (color == StoneColor.BLACK)
+			opposite = StoneColor.WHITE;
+		else
+			opposite = StoneColor.BLACK;
 	}
 
 	@Override
 	public StoneColor getColor() {
 		return player;
 	}
-	
+
 	@Override
 	public StoneColor getOppositeColor() {
 		if (getColor() == StoneColor.BLACK)
 			return StoneColor.WHITE;
-		else return StoneColor.BLACK;
+		else
+			return StoneColor.BLACK;
 	}
 
-
 	public Move layStone(Board board, int move, int numberOfStones) {
-		//panel.setRobotOnTurn(true);
+		// panel.setRobotOnTurn(true);
 		System.out.println("I'm thinking ... \n");
 
 		// Computer determines his best possible move
-		MinimaxResult result = Minmax.minmaxDecide(board, player, opposite,
-				1, move, numberOfStones);
+		MinimaxResult result = Minmax.minmaxDecide(board, player, opposite, 1,
+				move, numberOfStones);
 		return result.bestMove;
 
 	}
